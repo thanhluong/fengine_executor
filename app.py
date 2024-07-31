d540077efc651b0a369b141fcdd2371b18be0d38
thanhluong <doannguyenthanhluong@gmail.com>
2024-07-31 23:04:19 +0700

    Add error handling

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from dotenv import load_dotenv
 
@@ -9,6 +10,19 @@ from utils import rand_filename, b64e, b64d
 
 load_dotenv()
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 class CompileRequest(BaseModel):
